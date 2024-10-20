0674f5263d4bdd3bf879927ab4f74823b560b105
Andrei Gheorghita <andrey_890@yahoo.com>
2024-10-20 22:26:41 +0200

    Al treilea commit Proiect Cantina Facultate

--- cantina_facultate/cantina/views.py
+++ cantina_facultate/cantina/views.py
@@ -7,7 +7,17 @@ def prima_pagina(request):
 
 
 def meniu(request):
-    produse = Produs.objects.all()
+    produse = [
+        {'denumire': 'Ciorba rădăuțeană', 'pret': 3.5},
+        {'denumire': 'Ciorbă de perișoare', 'pret': 5.19},
+        {'denumire': 'Cascaval pane', 'pret': 4.55},
+        {'denumire': 'Chiftele cu carne și sos', 'pret': 6.5},
+        {'denumire': 'Grătar din piept de pui', 'pret': 8},
+        {'denumire': 'Mici cu muștar', 'pret': 2},
+        {'denumire': 'Tochitură moldovenească', 'pret': 10},
+        {'denumire': 'Pâine felie', 'pret': 0.25},
+        {'denumire': 'Mămăligă', 'pret': 0.25},
+    ]
     return render(request, 'cantina/meniu.html', {'produse': produse})
 
 
